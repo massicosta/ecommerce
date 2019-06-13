39ef7c6cded3a645873d654e0ac303f52a985b8f
Isaias Oliveira <isaias@butec.com.br>
2019-06-13 12:14:27 -0300

    criamos a tela de login

---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Hcode;
+
+class Model {
+
+    private $values = [];
+
+        public function __call($name, $args)
+    {
+
+        $method = substr($name, 0, 3);//pega 3 caracter começand da posição 0
+        $fieldName = substr($name, 3, strlen($name));//pega a quantidade de string do nome começando à partir da posição 3
+
+        switch ($method)
+        {
+            case "get":
+                return $this->values[$fieldName];
+            break;
+
+            case "set":
+                $this->values[$fieldName] = $args[0];
+            break;
+
+        }
+
+    }
+
+    public function setData($data = array())
+    {
+
+        foreach ($data as $key => $value){
+
+            $this->{"set".$key}($value);
+        }
+
+    }
+
+    public function getValues()
+    {
+
+        return $this->values;
+
+    }
+
+}
+
