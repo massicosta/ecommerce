6f92a3cb44b7c04befccebf0447a8812deca038b
Isaias Oliveira <isaias@butec.com.br>
2019-06-18 11:26:56 -0300

    Admin - produtos

---
+++ admin-categories.php
@@ -0,0 +1,106 @@
+<?php
+
+use Hcode\Model\Category;
+use Hcode\Page;
+use Hcode\PageAdmin;
+
+$app->get("/admin/categories", function (){
+
+    $categories = Category::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories", [
+        'categories'=>$categories
+    ]);
+
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category ->setData($_POST);
+
+    $category ->save();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category ->get((int)$idcategory);
+
+    $category ->delete();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update", [
+        "category"=>$category->getvalues()
+    ]);
+
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/categories/:idcategory", function ($idcategory)
+{
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new Page();
+
+    $page->setTpl("category", [
+        'category'=>$category->getValues(),
+        'products'=>[]
+    ]);
+
+
+});
