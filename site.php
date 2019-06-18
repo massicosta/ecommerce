06a82a0f7146e9968e50b69b570a39fc0fd69eb2
Isaias Oliveira <isaias@butec.com.br>
2019-06-18 18:07:36 -0300

    Paginação

--- site.php
+++ site.php
@@ -1,35 +1,31 @@
 <?php
-
-use Hcode\Model\Category;
-use Hcode\Page;
-use Hcode\Model\Product;
-
+use \Hcode\Page;
+use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 $app->get('/', function() {
 
     $products = Product::listAll();
-
     $page = new Page();
-
     $page->setTpl("index", [
-        'products'=>Product::checklist($products)
+        'products'=>Product::checkList($products)
     ]);
-
 });
-
-$app->get("/categories/:idcategory", function ($idcategory)
-{
-
+$app->get("/categories/:idcategory", function($idcategory){
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
     $category = new Category();
-
     $category->get((int)$idcategory);
-
+    $pagination = $category->getProductsPage($page);
+    $pages = [];
+    for ($i=1; $i <= $pagination['pages']; $i++) {
+        array_push($pages, [
+            'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+            'page'=>$i
+        ]);
+    }
     $page = new Page();
-
     $page->setTpl("category", [
         'category'=>$category->getValues(),
-        'products'=>Product::checklist($category->getProducts())
+        'products'=>$pagination["data"],
+        'pages'=>$pages
     ]);
-
-
 });
-
