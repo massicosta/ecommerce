77afa06139e0d32dbcb3d6ba7ba8a5c5b242ecc2
Isaias Oliveira <isaias@butec.com.br>
2019-06-26 11:02:34 -0300

    Minha conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -9,6 +9,8 @@ class User extends Model {
     const SECRET_IV = "HcodePhp7_Secret_IV";
     const ERROR = "UserError";
     const ERROR_REGISTER = "UserErrorRegister";
+    const SUCCESS = "UserSuccess";
+
     public static function getFromSession()
     {
         $user = new User();
@@ -229,6 +231,21 @@ class User extends Model {
     public static function clearError()
     {
         $_SESSION[User::ERROR] = NULL;
+    }
+
+    public static function setSuccess($msg)
+    {
+        $_SESSION[User::SUCCESS] = $msg;
+    }
+    public static function getSuccess()
+    {
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+        User::clearError();
+        return $msg;
+    }
+    public static function clearSuccess()
+    {
+        $_SESSION[User::SUCCESS] = NULL;
     }
 
     public static function setErrorRegister($msg)
